d69580dcca19890e7f90cd77293adbac3b1196a9
Link1515 <h26281660@gmail.com>
2024-04-15 09:59:27 +0800

    rewrite array reverse

--- src/ArrayReverse.java
+++ src/ArrayReverse.java
@@ -1,15 +1,18 @@
 import java.util.Arrays;
 
 public class ArrayReverse {
+
   public static void main(String[] args) throws Exception {
-    int[] arr = { 1, 2, 3, 4, 5, 6 };
+    int[] nums = { 1, 2, 3, 4, 5, 6 };
+    arrayReverse(nums);
+    System.out.println(Arrays.toString(nums));
+  }
 
-    for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
-      int temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
+  public static void arrayReverse(int[] nums) {
+    for (int i = 0, j = nums.length - 1; i < j; i++, j--) {
+      int temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
     }
-
-    System.out.println(Arrays.toString(arr));
   }
 }

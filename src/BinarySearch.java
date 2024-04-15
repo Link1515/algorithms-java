758b721d097fafd28fffdb1d1abd335648bf9c30
Link1515 <h26281660@gmail.com>
2024-04-15 09:51:39 +0800

    rewrite binery search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,36 +1,29 @@
 public class BinarySearch {
+
   public static void main(String[] args) throws Exception {
-    int[] arr = { 2, 5, 8, 23, 32, 69, 79, 98, 105 };
+    int[] nums = { 2, 5, 8, 23, 32, 69, 79, 98, 105 };
+    int target = 23;
 
-    int target = 105;
+    int targetIndex = binarySearch(nums, target);
+    System.out.println(targetIndex);
+  }
 
-    int start = 0;
-    int end = arr.length - 1;
+  public static int binarySearch(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
 
-    int targetIndex = -1;
-    while (end > start) {
-      int mid = (start + end) / 2;
+    while (left <= right) {
+      int mid = (left + right) / 2;
 
-      if (arr[mid] == target) {
-        targetIndex = mid;
-        break;
-      } else if (arr[mid] > target) {
-        end = mid - 1;
-        if (end == 0 && target == arr[0]) {
-          targetIndex = 0;
-        }
+      if (nums[mid] > target) {
+        right = mid - 1;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
       } else {
-        start = mid + 1;
-        if (start == arr.length - 1 && target == arr[arr.length - 1]) {
-          targetIndex = arr.length - 1;
-        }
+        return mid;
       }
     }
 
-    if (targetIndex >= 0) {
-      System.out.println("Found target at: " + targetIndex);
-    } else {
-      System.out.println("Target not found");
-    }
+    return -1;
   }
 }

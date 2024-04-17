de767612530d187881f8942513222b68eb7dfcbd
Link1515 <h26281660@gmail.com>
2024-04-17 09:13:37 +0800

    add merge sort

---
+++ src/sort/MergeSort.java
@@ -0,0 +1,51 @@
+package sort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeSort {
+
+  public static void main(String[] args) {
+    List<Integer> sortedNums = mergeSort(Arrays.asList(3, 2, 6, 9, 3));
+    System.out.println(sortedNums);
+  }
+
+  public static List<Integer> mergeSort(List<Integer> nums) {
+    if (nums.size() == 1) {
+      return nums;
+    }
+    int mid = nums.size() / 2;
+    List<Integer> leftHalfNums = new ArrayList<>(nums.subList(0, mid));
+    List<Integer> rightHalfNums = new ArrayList<>(
+      nums.subList(mid, nums.size())
+    );
+
+    return mergeTwoSortedArr(mergeSort(leftHalfNums), mergeSort(rightHalfNums));
+  }
+
+  public static List<Integer> mergeTwoSortedArr(
+    List<Integer> list1,
+    List<Integer> list2
+  ) {
+    ArrayList<Integer> result = new ArrayList<>();
+
+    while (list1.size() > 0 && list2.size() > 0) {
+      if (list1.get(0) < list2.get(0)) {
+        result.add(list1.remove(0));
+      } else {
+        result.add(list2.remove(0));
+      }
+    }
+
+    if (list1.size() > 0) {
+      result.addAll(list1);
+    }
+
+    if (list2.size() > 0) {
+      result.addAll(list2);
+    }
+
+    return result;
+  }
+}
